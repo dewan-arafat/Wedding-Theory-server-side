8060dfada59739f623a46cef4bb7bec42b684712
dewan-arafat <dewanarafat1@gmail.com>
2022-11-09 14:02:10 +0600

    server set up and database connection

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.awyxyaj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('wedding server is running')
+})
+
+app.listen(port, () => {
+    console.log(`wedding server running on ${port}`);
+})
