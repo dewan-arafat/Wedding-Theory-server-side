1f820203fd9a5df0a820591706ce96a0fdb5225c
dewan-arafat <dewanarafat1@gmail.com>
2022-11-09 18:37:24 +0600

    3 services fetch from database

--- index.js
+++ index.js
@@ -12,6 +12,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.awyxyaj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('weddingPhoto').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+
+
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(err => console.error(err));
+
+
 
 
 
